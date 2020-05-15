daa705995f1e5ef4e6e3cccac42566ce5017af27
João Henrique Andrade <andradejoaoh@gmail.com>
2020-05-15 16:55:30 -0300

    This is the initial view of the application in the first stage

--- MiniRemoto/SceneDelegate.swift
+++ MiniRemoto/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         window = UIWindow(windowScene: scene)
         window?.makeKeyAndVisible()
-        window?.rootViewController = TextWidgetViewController()
+        window?.rootViewController = CanvasCollectionViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
