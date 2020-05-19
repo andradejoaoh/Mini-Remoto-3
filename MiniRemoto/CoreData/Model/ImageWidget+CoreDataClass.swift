2721c5161754948eea52707cd7be7d179f0dafff
João Henrique Andrade <andradejoaoh@gmail.com>
2020-05-19 17:22:55 -0300

    Core Data primitivo, salvando e deletando objetos

---
+++ MiniRemoto/CoreData/Model/ImageWidget+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  ImageWidget+CoreDataClass.swift
+//  MiniRemoto
+//
+//  Created by João Henrique Andrade on 19/05/20.
+//  Copyright © 2020 João Henrique Andrade. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ImageWidget)
+public class ImageWidget: NSManagedObject {
+
+}
