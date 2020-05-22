0e802eb707ca52afa1f367599f5a2c115dc06cd9
Galdineris <galdineris@gmail.com>
2020-05-22 18:16:28 -0300

    Add Icon in WidgetData and implement make() in compliant structs

--- MiniRemoto/Widgets/WidgetData.swift
+++ MiniRemoto/Widgets/WidgetData.swift
@@ -18,7 +18,14 @@ enum WidgetType: String {
 protocol WidgetData {
     var type: WidgetType { get }
     var frame: CGRect { get set }
+    var iconPath: String { get set }
     func make() -> WidgetView
+}
+
+extension WidgetData {
+    var iconImage: UIImage {
+        return UIImage(systemName: self.iconPath) ?? UIImage()
+    }
 }
 
 struct TextWidgetModel: WidgetData {
@@ -26,6 +33,7 @@ struct TextWidgetModel: WidgetData {
     var frame: CGRect
     var title: String
     var body: String
+    var iconPath: String = "pencil.circle.fill"
 
     init(frame: CGRect = CGRect.zero, title: String = "", body: String = "") {
         self.type = WidgetType.text
@@ -33,17 +41,28 @@ struct TextWidgetModel: WidgetData {
         self.title = title
         self.body = body
     }
+
+    func make() -> WidgetView {
+        let controller = TextWidgetController()
+        return TextWidgetView(controller: controller)
+    }
 }
 
 struct ImageWidgetModel: WidgetData {
     var type: WidgetType
     var frame: CGRect
     var image: UIImage
+    var iconPath: String = "camera.on.rectangle.fill"
 
     init(frame: CGRect = CGRect.zero, image: UIImage = UIImage()) {
         self.type = WidgetType.text
         self.frame = frame
         self.image = image
     }
+
+    func make() -> WidgetView {
+        return ImageWidgetView(image: self.image)
+    }
+
 }
 
